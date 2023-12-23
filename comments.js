c7032f0f097649ddecfa534cc423f46fb6e93053
Shagun Sodhani <1321193+shagunsodhani@users.noreply.github.com>
2023-12-23 17:13:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,46 @@
-// create web server 
-// 1. create web server
-// 2. create router
-// 3. create controller
-// 4. create model
+// Create web server
 
+// 1. Import Express
 const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/comments');
-const auth = require('../middleware/auth');
+const bodyParser = require('body-parser');
+const app = express();
 
-router.post('/addComment', auth, commentController.addComment);
-router.get('/getComments', commentController.getComments);
-router.get('/getComments/:id', commentController.getCommentsById);
-router.put('/updateComment/:id', auth, commentController.updateComment);
-router.delete('/deleteComment/:id', auth, commentController.deleteComment);
+// 2. Setup body-parser
+app.use(bodyParser.urlencoded({extended: false}));
 
-module.exports = router;
+// 3. Setup template engine
+app.set('view engine', 'ejs');
+
+// 4. Setup static folder
+app.use(express.static('public'));
+
+// 5. Setup comments array
+const comments = [
+    {name: 'Minh', content: 'Hello'},
+    {name: 'Minh', content: 'Hello'},
+    {name: 'Minh', content: 'Hello'}
+];
+
+// 6. Setup route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/comments', (req, res) => {
+    res.render('comments', {comments: comments});
+});
+
+app.post('/comments', (req, res) => {
+    const newComment = {
+        name: req.body.name,
+        content: req.body.content
+    };
+    comments.push(newComment);
+    res.redirect('comments');
+});
+
+// 7. Start server
+app.listen(3000, (err) => {
+    if (err) console.log(err);
+    else console.log('Server is listening at port 3000');
+});
